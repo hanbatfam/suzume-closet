f9cc430f1a02c1c25b28f23f3b34010a5b19398b
taekto <taekputer@gmail.com>
2023-06-22 15:30:06 +0900

    [FEAT/back] : Create Board domain

---
+++ backend/src/main/java/com/suzume/closet/domain/Board.java
@@ -0,0 +1,29 @@
+package com.suzume.closet.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+@Getter
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+    private String content;
+    private Long viewcount;
+
+
+}
