036013a0a009e9ce12b8dd53e9e32c44753f1516
taekto <taekputer@gmail.com>
2023-06-22 15:40:23 +0900

    [CHORE/back] : Adjust Convention Board Domain

--- backend/src/main/java/com/suzume/closet/domain/Board.java
+++ backend/src/main/java/com/suzume/closet/domain/Board.java
@@ -6,24 +6,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Entity
 @Getter
 public class Board {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
     private String title;
+
     private String content;
+
     private Long viewcount;
-
-
 }
