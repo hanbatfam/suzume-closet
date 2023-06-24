d8b9cf1b485cdbd5bb0ff853574effca33e638c5
bigstar017 <335005@naver.com>
2023-06-24 14:57:06 +0900

    [CHORE/back] : Delete userOuterList

--- crawling/src/main/java/com/suzume/crawling/domain/Outer.java
+++ crawling/src/main/java/com/suzume/crawling/domain/Outer.java
@@ -28,7 +28,4 @@ public class Outer {
 
     @OneToMany(mappedBy = "outer")
     private List<Coordi> coordiList;
-
-    @OneToMany(mappedBy = "outer")
-    private List<UserOuter> userOuterList;
 }
