889e1ebf467c0104ce6c11daab7e0bb3e5effdc3
taekto <taekputer@gmail.com>
2023-06-26 21:50:30 +0900

    [SC-22/CHORE/back] : Adjust import Convention

--- backend/src/main/java/com/suzume/closet/repository/FollowRepository.java
+++ backend/src/main/java/com/suzume/closet/repository/FollowRepository.java
@@ -1,7 +1,6 @@
 package com.suzume.closet.repository;
 
 import com.suzume.closet.domain.Follow;
-import com.suzume.closet.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
